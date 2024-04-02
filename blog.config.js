124de1ead5fad1a80fc47b45e89a3d775dca960a
whiteOrdinary <164706354+whiteOrdinary@users.noreply.github.com>
2024-04-02 13:58:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Small Ambition', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'japan guides', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'japan guides🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://japan-guides.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'japan-guides, japan, guides', // 网站关键词 英文逗号隔开
