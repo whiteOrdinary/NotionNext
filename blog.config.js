b72061984c70bf811f52a57d0ce102f555c94f1b
whiteOrdinary <164706354+whiteOrdinary@users.noreply.github.com>
2024-05-01 11:32:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -60,7 +60,7 @@ const BLOG = {
   ],
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
-    // '"LXGW WenKai"',
+    'Tahoma',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
@@ -80,7 +80,7 @@ const BLOG = {
   ],
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
-    // '"LXGW WenKai"',
+    'Tahoma',
     'Bitter',
     '"Noto Serif SC"',
     'SimSun',
