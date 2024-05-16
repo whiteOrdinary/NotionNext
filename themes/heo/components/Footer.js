8a4644cae2e0c360134abb02ea086f8b6e38b31d
whiteOrdinary <164706354+whiteOrdinary@users.noreply.github.com>
2024-05-16 10:13:46 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -27,8 +27,8 @@ const Footer = ({ title }) => {
             {/* 底部页面信息 */}
             <div id='footer-bottom' className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#30343f]'>
 
-                <div id='footer-bottom-left'>
-                   <i className='fas fa-copyright' /> {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' /> <a href={siteConfig('LINK')} className='underline font-bold  dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>.
+               <div id='footer-bottom-left'>
+                   <i className='fas fa-copyright' /> {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' /> <a href={siteConfig('LINK')} className='underline font-bold  dark:text-gray-300 '>{siteConfig('AUTHOR')}</a> | <a href='https://japan-guides.com/privacy' target='_blank' className='underline font-bold  dark:text-gray-300 '>Privacy Policy</a> | <a href='https://japan-guides.com/about' target='_blank' className='underline font-bold  dark:text-gray-300 '>About US</a>
                 </div>
 
             <div id='footer-bottom-right'>
