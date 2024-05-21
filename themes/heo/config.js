cefae9f037857cd5833ca3a5e5935378c99e70df
whiteOrdinary <164706354+whiteOrdinary@users.noreply.github.com>
2024-05-21 10:05:02 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -55,8 +55,8 @@ const CONFIG = {
       color_2: '#ffffff'
     },
     {
-      title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
+      title_1: 'Kamakura',
+      img_1: '/images/heo/22.png',
       color_1: '#57b6e6',
       title_2: 'Photoshop',
       img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
